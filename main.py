2ee55cbe8f93f6879e9b9333d37d72d87b921bd8
baraoo22 <baraoo22@gmail.com>
2026-02-11 17:06:41 +0000

    main.py

--- main.py
+++ main.py
@@ -46,3 +46,9 @@ def perguntar(p: Pergunta):
 
     return {"resposta": resposta}
 
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
